990412b5ab25033f238d1fae6ae50a8698d1ce0a
Gabe-GH <gabriel.lira01@utrgv.edu>
2022-10-16 00:10:15 -0500

    added descriptions for easy read

--- webScraper/devPost.js
+++ webScraper/devPost.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer')
 
-async function goToURL(userName){
+// puppeteer will go to a user's devpost profile
+//
+// returns page for access of the page's elements
+// returns browser for ability to close the browser
+// returns response for ability to access the page's response code for error handling
+async function goToURL(userName) {
     const userURI = `https://devpost.com/${userName}`;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -9,17 +14,22 @@ async function goToURL(userName){
     return [page, browser, response]
 }
 
+// if a user's profile function will scrape each of the user's project links
+// returns an array of strings containing links to each project
+// 
+// else if a user's profile isn't found will return an error
+// throws an error
 async function scrapeUserProjectLinks(userName) {
-    
+
     const [page, browser, response] = await goToURL(userName)
 
-    if(response.status() == 404){
+    if (response.status() == 404) {
         await browser.close()
-        throw new Error({'message': 'username not found'})
+        throw new Error({ 'message': 'username not found' })
     }
 
     const scrapeProjects = await page.evaluate(() => {
-        
+
         const softwareEntries = document.querySelectorAll(".link-to-software");
         let projects = []
 
@@ -31,7 +41,6 @@ async function scrapeUserProjectLinks(userName) {
     })
 
     await browser.close();
-
     return scrapeProjects;
 }
 
